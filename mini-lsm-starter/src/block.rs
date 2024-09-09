1b4c3fd0a96b1049cdb2dcff99409b7792f73d1a
Elainasama <1223530014@qq.com>
2024-09-09 23:01:21 +0800

    week1day3

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -5,7 +5,7 @@ mod builder;
 mod iterator;
 
 pub use builder::BlockBuilder;
-use bytes::Bytes;
+use bytes::{BufMut, Bytes};
 pub use iterator::BlockIterator;
 
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted key-value pairs.
@@ -18,11 +18,37 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the tutorial
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let mut data = self.data.clone();
+        let offsets = self.offsets.clone();
+        for offset in offsets {
+            data.put_u16(offset);
+        }
+        data.put_u16(self.offsets.len() as u16);
+        data.into()
+    }
+
+    // 获取字段长度u16
+    pub fn get_u16(data: &[u8], idx: usize) -> u16 {
+        let high = data[idx];
+        let low = data[idx + 1];
+        ((high as u16) << 8) + low as u16
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let mut offsets = Vec::new();
+        let mut cur_idx = data.len() - 2;
+        // 获取元素总长度
+        let num_entry = Self::get_u16(data, cur_idx);
+        dbg!(num_entry);
+        for _ in 0..num_entry {
+            cur_idx -= 2;
+            offsets.push(Self::get_u16(data, cur_idx));
+        }
+        offsets.reverse();
+        Self {
+            data: data[0..cur_idx].to_vec(),
+            offsets,
+        }
     }
 }
--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -20,22 +20,60 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: vec![],
+            data: vec![],
+            block_size,
+            first_key: KeyVec::new(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        // 第一个插入
+        let offset = self.data.len() as u16;
+        self.add_to_offset(key, value, offset)
+    }
+
+    fn cal_block_size(&self) -> usize {
+        // Data Section | Offset Section |  Extra |
+        self.data.len() + self.offsets.len() * 2 + 2
+    }
+
+    fn add_to_offset(&mut self, key: KeySlice, value: &[u8], offset: u16) -> bool {
+        // 键值太长
+        if key.len() > 0xffff || value.len() > 0xffff {
+            return false;
+        }
+        // 除非第一个键值对超出目标块大小，否则应确保编码后的块大小始终小于或等于target_size。
+        // 新增key_len key val_len value offset
+        if offset > 0 && self.cal_block_size() + key.len() + value.len() + 4 + 2 > self.block_size {
+            return false;
+        }
+        self.offsets.push(offset);
+        let key_len = key.len() as u16;
+        let val_len = value.len() as u16;
+
+        self.data
+            .extend(vec![((key_len >> 8) & 0xff) as u8, (key_len & 0xff) as u8]);
+        self.data.extend(key.into_inner());
+        self.data
+            .extend(vec![((val_len >> 8) & 0xff) as u8, (val_len & 0xff) as u8]);
+        self.data.extend(value.to_vec());
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.data.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
